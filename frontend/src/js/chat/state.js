51e6afeaa439712ae398d66ff3897dc4e3b9096d
Pratik Bakshi <bakshi.pratikcool@gmail.com>
2025-12-31 17:58:39 +0530

    style issue resolved

--- frontend/src/js/chat/state.js
+++ frontend/src/js/chat/state.js
@@ -1,13 +1,3 @@
-// export const state = {
-//   token: localStorage.getItem("token"),
-//   user: JSON.parse(localStorage.getItem("user")),
-//   roomData: [],
-//   currentRoomId: null,
-//   currentRoomName: null,
-// hasJoined: false,
-//   messages: [],
-// };
-
 export const state = {
   token: localStorage.getItem("token"),
   user: JSON.parse(localStorage.getItem("user")) || null,
